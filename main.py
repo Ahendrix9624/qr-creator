9e9c870f359861f4f20fe36939d32bfd84790c4f
Drew <33384698+Ahendrix9624@users.noreply.github.com>
2023-03-11 14:24:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+USAGE - The code generates a QR code for a given website link (here, the YouTube channel link) 
+        using the qrcode library. It creates an instance of QRCode, adds the website link to it, 
+        and generates an image of the QR code with the specified box size, border, and color 
+        using make_image() function. Finally, it saves the generated image as 'qrcode001.png' in 
+        the current working directory.
+
+AUTHOR - https://github.com/Ahendrix9624/
+"""
 import qrcode
 # Link for website
 input_data = "https://www.youtube.com/channel/UCWA6QltSggvOm49y9Mx_vPQ"
